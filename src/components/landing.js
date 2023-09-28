0bb93992a57522b45c87949b507d9ecfd1386728
RonanJasperReponte <jasperreponte19@gmail.com>
2023-09-28 18:38:15 +0800

    v1.141
    updated landing pagr

--- src/components/landing.js
+++ src/components/landing.js
@@ -11,49 +11,65 @@ import Button from 'react-bootstrap/Button';
 function Home() {
 
   // State variable to track whether the delay is complete
-  const [delayComplete, setDelayComplete] = useState(false);
+  // const [delayComplete, setDelayComplete] = useState(false);
 
-  // Simulate a 5-second delay before showing content
-  useEffect(() => {
-    const delayTimer = setTimeout(() => {
-      setDelayComplete(true);
-    }, 5000);
+  // // Simulate a 5-second delay before showing content
+  // useEffect(() => {
+  //   const delayTimer = setTimeout(() => {
+  //     setDelayComplete(true);
+  //   }, 5000);
 
-    // Clean up the timer if the component unmounts
-    return () => clearTimeout(delayTimer);
-  }, []);
+  //   // Clean up the timer if the component unmounts
+  //   return () => clearTimeout(delayTimer);
+  // }, []);
 
   // Render loading skeleton during the delay
-  if (!delayComplete) {
-    return (
-      <header className="App-header">
-        <Skeleton height={300} width={300} circle={true} />
-        <Skeleton height={30} width={500} />
-        <Skeleton height={30} width={300} />
-        <Skeleton height={30} width={300} />
-      </header>
-    );
-  }
+  // if (!delayComplete) {
+  //   return (
+  //     <header className="App-header">
+  //       <Skeleton height={300} width={300} circle={true} />
+  //       <Skeleton height={30} width={500} />
+  //       <Skeleton height={30} width={300} />
+  //       <Skeleton height={30} width={300} />
+  //     </header>
+  //   );
+  // }
 
   // After the delay, display the content
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>CREATE YOUR OWN WISHLIST!</h1>
       <Form className='justify-content-end'>
             <Row>
-            <Col xs="auto">
+            <Col xs='19'>
                 <Form.Control
                 type="text"
                 placeholder="Search for discounted products!"
                 className=" mr-sm-2"
                 />
             </Col>
-            <Col xs="auto">
+            <Col>
                 <Button type="submit">Search</Button>
             </Col>
             </Row>
         </Form>
+      <h1>CREATE YOUR OWN WISHLIST!</h1>
+      <p>Add items to your Wishlist and be notified of any discounts!</p>
+      <Row>
+      <Col>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>ADD</p>
+      </Col>
+      <Col>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>CHECKOUT</p>
+      </Col>
+      <Col>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>INFORM</p>
+      </Col>
+      </Row>
+      
+        <Button href='/register'>CREATE YOUR WISHLIST NOW!</Button>
     </header>
   );
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Nav from 'react-bootstrap/Nav';
 import '../styles/sidebar.css'
+import Col from 'react-bootstrap/Col';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+            <Nav className="me-auto flex-column">
+                <Col>
+                    <Nav.Link className="link fs-3" href="home">MY LIST</Nav.Link>
+                </Col>
+                <Col>
+                    <Nav.Link className="link fs-3" href="about">RECENT</Nav.Link>
+                </Col>
+          </Nav>
         </div>
     );
 }
